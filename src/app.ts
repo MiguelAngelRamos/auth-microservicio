227c69a1b5634621411e49f8ab7ad3cbf0926cb4
Miguel Ramos <return.infox@gmail.com>
2024-02-07 17:03:03 -0300

    codigo del api terminado antes de Hydra

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,33 @@
+import express from 'express';
+import { loadControllers, scopePerRequest } from 'awilix-express';
+import container from './config/container';
+import connectDB from './config/mongo';
+
 import dotenv from 'dotenv';
 dotenv.config();
+
+const startServer = async () => {
+  const app = express();
+  app.use(express.json());
+  app.use(scopePerRequest(container));
+
+  // Conexion a la base de datos
+  connectDB();
+
+  app.use(loadControllers(
+    'controllers/*.ts',
+    { cwd: __dirname }
+  ));
+
+  app.listen(process.env.PORT, ()=> {
+    console.log(`Server listening on port ${process.env.PORT}`);
+  });
+};
+
+startServer().catch(err => {
+  console.error('Error starting server', err);
+  process.exit(1);
+});
+
+
+export default startServer;
