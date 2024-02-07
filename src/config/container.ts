5c8484c6858c06fe80e7337a924acfa6bf2ae367
Miguel Ramos <return.infox@gmail.com>
2024-02-07 15:57:11 -0300

    modelo de mongo terminado

---
+++ src/config/container.ts
@@ -0,0 +1,12 @@
+import { createContainer, InjectionMode } from "awilix";
+
+
+const container = createContainer({
+  injectionMode: InjectionMode.CLASSIC
+});
+
+container.register({
+
+});
+
+export default container;
